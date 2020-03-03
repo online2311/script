424f8cbc5590180117ef22f425f472f691aa736d
ZhangJing <13821320100@outlook.com>
2020-03-03 22:02:49 +0800

    Create gost-update.sh

---
+++ gost-update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Usage:
+#   curl https://raw.githubusercontent.com/online2311/script/master/gost-update.sh | bash
+
+METHOD="-C /etc/gost.json"
+
+VER="$(wget -qO- https://github.com/ginuerzh/gost/tags | grep -oE "/tag/v.*" | sed -n '1p' | sed 's/\".*//;s/^.*v//')"
+VER=${VER:=2.11.0}
+URL="https://github.com/ginuerzh/gost/releases/download/v${VER}/gost-linux-amd64-${VER}.gz"
+
+echo "Downloading gost_${VER} to /usr/bin/gost from $URL"
+rm -rf /usr/bin/gost
+wget -O /usr/bin/gost-linux-amd64-${VER}.gz $URL
+gzip -d /usr/bin/gost-linux-amd64-${VER}.gz && 
+cd /usr/bin/ && systemctl stop gost.service && mv gost-linux-amd64-${VER} /usr/bin/gost && chmod +x /usr/bin/gost  && systemctl start gost.service && systemctl status gost -l
