41689e7ea0163ef74f945d71c581ccd6fb717b5c
ZhangJing <13821320100@outlook.com>
2020-03-03 22:09:04 +0800

    Update gost-update.sh

--- gost-update.sh
+++ gost-update.sh
@@ -9,7 +9,7 @@ VER=${VER:=2.11.0}
 URL="https://github.com/ginuerzh/gost/releases/download/v${VER}/gost-linux-amd64-${VER}.gz"
 
 echo "Downloading gost_${VER} to /usr/bin/gost from $URL"
-rm -rf /usr/bin/gost
+rm -rf /usr/bin/gost-linux-amd64*
 wget -O /usr/bin/gost-linux-amd64-${VER}.gz $URL
 gzip -d /usr/bin/gost-linux-amd64-${VER}.gz && 
-cd /usr/bin/ && systemctl stop gost.service && mv gost-linux-amd64-${VER} /usr/bin/gost && chmod +x /usr/bin/gost  && systemctl start gost.service && systemctl status gost -l
+cd /usr/bin/ && systemctl stop gost.service && rm -rf /usr/bin/gost && mv gost-linux-amd64-${VER} /usr/bin/gost && chmod +x /usr/bin/gost  && systemctl start gost.service && systemctl status gost -l
